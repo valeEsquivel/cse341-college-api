50a243070db427b2070041118fb55a3c7576a2c3
Valeria Esquivel <esq22001@byui.edu>
2025-06-02 21:41:22 -0600

    Initial Message

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     //#swagger.tags = ['Hello World']
-    res.send('Welcome to the home page!');
+    res.send('Welcome to the home page of College API!');
 })
 
 //Add the files to the routes we are going to use
