2c100bcdd7a53d2a0c77b6329055e9473b3577ef
E-I-M-C <mormons.at@gmail.com>
2025-06-03 17:29:30 -0700

    Added users route and controller

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    //#swagger.tags = ['Hello World']
-    res.send('Welcome to the home page of College API!');
-})
+    //#swagger.tags = ['Welcome']
+    res.send('Welcome to CSE341 College API!');
+});
 
 //Add the files to the routes we are going to use
+router.use('/user', require('./users'));
 
 module.exports = router;
