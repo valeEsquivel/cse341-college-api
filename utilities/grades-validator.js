f2b68876676a681fc8bf4364b5b9d601917f1933
Valeria Esquivel <esq22001@byui.edu>
2025-06-12 22:59:24 -0600

    Modify validations

--- utilities/grades-validator.js
+++ utilities/grades-validator.js
@@ -1,71 +1,15 @@
-const { body, validationResult } = require("express-validator");
-const gradeController = require("../controllers/grades");
-const validate = {};
+const { body, validationResult } = require('express-validator');
 
-/* ***********************
- * Data Validation Rules
- *************************/
-validate.gradesRules = () => {
+const gradesRules = () => {
   return [
-    // term: required, string type, not empty
-    body("term")
-      .exists()
-      .bail()
-      .isString()
-      .bail()
-      .notEmpty()
-      .trim()
-      .escape(),
-    
-      // grade: required, string type, not empty
-    body("grade")
-      .exists()
-      .bail()
-      .isString()
-      .bail()
-      .notEmpty()
-      .trim()
-      .escape(),
-
-    //studentId: required, string type, not empty, custom validator
-    body("studentId")
-      .exists()
-      .bail()
-      .isString()
-      .bail()
-      .notEmpty()
-      .bail()
-      .custom(async (value) => {
-        const studentExists = await gradeController.verifyStudent(value);
-        if (!studentExists) {
-          throw new Error("Student ID does not exist.");
-        }
-        return true;
-      })
-      .trim()
-      .escape(),
-
-    //courseId: required, string type, not empty, custom validator
-    body("courseId")
-      .exists()
-      .bail()
-      .isString()
-      .bail()
-      .notEmpty()
-      .bail()
-      .custom(async (value) => {
-        const courseExists = await gradeController.verifyCourse(value);
-        if (!courseExists) {
-          throw new Error("Course ID does not exist.");
-        }
-        return true;
-      })
-      .trim()
-      .escape(),
+    body('term').isString().notEmpty(),
+    body('grade').isString().notEmpty(),
+    body('studentId').isString().notEmpty(),
+    body('courseId').isString().notEmpty(),
   ];
-};
+}
 
-validate.validateGrade = (req, res, next) => {
+const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
     return next();
@@ -76,6 +20,9 @@ validate.validateGrade = (req, res, next) => {
   return res.status(422).json({
     errors: extractedErrors
   });
-};
+}
 
-module.exports = validate;
+module.exports = {
+  gradesRules,
+  validate
+};
--- utilities/teachers-validator.js
+++ utilities/teachers-validator.js
@@ -1,43 +1,14 @@
-const { body, validationResult } = require("express-validator");
-const validate = {};
+const { body, validationResult } = require('express-validator');
 
-/* ***********************
- * Data Validation Rules
- *************************/
-validate.teachersRules = () => {
+const teacherRules = () => {
   return [
-    // firstName: required, string, not empty, trimmed, escaped
-    body("firstName")
-      .exists()
-      .bail()
-      .isString()
-      .bail()
-      .notEmpty()
-      .trim()
-      .escape(),
-
-    // lastName: required, string, not empty, trimmed, escaped
-    body("lastName")
-      .exists()
-      .bail()
-      .isString()
-      .bail()
-      .notEmpty()
-      .trim()
-      .escape(),
-
-    // email: required, string type, not empty, valid email format
-    body("email")
-      .trim()
-      .bail()
-      .escape()
-      .notEmpty()
-      .isEmail()
-      .normalizeEmail(),
+    body('firstName').isString().notEmpty(),
+    body('lastName').isString().notEmpty(),
+    body('email').isEmail().notEmpty(),
   ];
-};
+}
 
-validate.validateTeacher = (req, res, next) => {
+const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
     return next();
@@ -48,6 +19,9 @@ validate.validateTeacher = (req, res, next) => {
   return res.status(422).json({
     errors: extractedErrors
   });
-};
+}
 
-module.exports = validate;
+module.exports = {
+  teacherRules,
+  validate
+};
