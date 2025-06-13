1d875a4931052d8b917e044740ee695ad6ecc401
Valeria Esquivel <esq22001@byui.edu>
2025-06-12 23:17:04 -0600

    Fix of validations in routes

--- routes/grades.js
+++ routes/grades.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
 const gradeController = require('../controllers/grades');
 const { authCheck } = require('../authentication/authenticate.js');
-const gradeValidator = require('../utilities/grades-validator')
+const { gradesRules, validate } = require('../utilities/grades-validator.js');
+
 
 router.get('/', gradeController.getAll);
 
 router.get('/:studentId', gradeController.getGradesByStudentId);
 
-router.post('/', authCheck, gradeValidator.gradesRules(), gradeValidator.validate, gradeController.addGrade);
+router.post('/', authCheck, gradesRules(), validate, gradeController.addGrade);
 
-router.put('/:gradeId', authCheck, gradeValidator.gradesRules(), gradeValidator.validate, gradeController.updateGrade);
+router.put('/:gradeId', authCheck, gradesRules(), validate, gradeController.updateGrade);
 
 router.delete('/:gradeId', authCheck, gradeController.deleteGrade);
 
--- routes/teachers.js
+++ routes/teachers.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
 const teacherController = require('../controllers/teachers');
 const { authCheck } = require('../authentication/authenticate.js');
-const teacherValidator = require('../utilities/teachers-validator')
-
+const { teacherRules, validate } = require('../utilities/teachers-validator.js');
 
 router.get('/', teacherController.getAll);
 
 router.get('/:teacherId', teacherController.getById);
 
-router.post('/', authCheck, teacherValidator.teachersRules(), teacherValidator.validate, teacherController.createTeacher);
+router.post('/', authCheck, teacherRules(), validate, teacherController.createTeacher);
 
-router.put('/:teacherId', authCheck, teacherValidator.teachersRules(), teacherValidator.validate, teacherController.updateTeacher);
+router.put('/:teacherId', authCheck, teacherRules(), validate, teacherController.updateTeacher);
 
 router.delete('/:teacherId', authCheck, teacherController.deleteTeacher);
 
